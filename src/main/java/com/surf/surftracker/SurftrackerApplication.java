a11af2cd53bbf54ff8227a4c71960062489640a3
Trevor <trevordmooney@gmail.com>
2024-08-25 15:56:02 -0700

    Base foundation for surf tracker

---
+++ src/main/java/com/surf/surftracker/SurftrackerApplication.java
@@ -0,0 +1,13 @@
+package com.surf.surftracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurftrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurftrackerApplication.class, args);
+	}
+
+}
