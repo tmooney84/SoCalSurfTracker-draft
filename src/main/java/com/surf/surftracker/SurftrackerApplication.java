2b4673fc37ea543904f392b1be37ed9168ea7c5a
Trevor <trevordmooney@gmail.com>
2024-08-30 21:38:22 -0700

    working on wind

--- src/main/java/com/surf/surftracker/SurftrackerApplication.java
+++ src/main/java/com/surf/surftracker/SurftrackerApplication.java
@@ -25,13 +25,15 @@ public class SurftrackerApplication implements CommandLineRunner {
 
 		@Override
 		public void run(String... args) throws Exception {
+
+		//adjust Current object to include SurfSpotID and surfSpotName
 		Current lowerTrestlesCurrent = new Current();
 
 		//right now hard coded with the trestles related url
 		//pass in the url as a string to be used within each of these services
-		SurfLine_rating_Service sl_rating_service = new SurfLine_rating_Service();
-		SurfLine_sunlight_Service sl_sunlight_service = new SurfLine_sunlight_Service();
-		SurfLine_surf_Service sl_surf_service = new SurfLine_surf_Service();
+		SurfLine_rating_Service lt_rating_service = new SurfLine_rating_Service();
+		SurfLine_sunlight_Service lt_sunlight_service = new SurfLine_sunlight_Service();
+		SurfLine_surf_Service lt_surf_service = new SurfLine_surf_Service();
 		SurfLine_swells_Service lt_swells_service = new SurfLine_swells_Service();
 		SurfLine_tides_Service lt_tides_service = new SurfLine_tides_Service();
 		SurfLine_weather_Service lt_weather_service = new SurfLine_weather_Service();
@@ -42,26 +44,29 @@ public class SurftrackerApplication implements CommandLineRunner {
 
 		try {
 			//once the services are run, I could implement them here as a list
-			SurfLine_rating_DTO ratingDTO = sl_rating_service.getSurfLineRating();
-			SurfLine_sunlight_DTO sunlightDTO = sl_sunlight_service.getSurfLineSunlight();
+			SurfLine_rating_DTO ratingDTO = lt_rating_service.getSurfLineRating();
+			SurfLine_sunlight_DTO sunlightDTO = lt_sunlight_service.getSurfLineSunlight();
+			SurfLine_surf_DTO surfDTO = lt_surf_service.getSurfLineSurf();
+			SurfLine_wind_DTO windDTO = lt_wind_service.getSurfLineWind();
 
 
-			CurrentMapper ltCurrentMapper = new CurrentMapper(lowerTrestlesCurrent,ratingDTO,sunlightDTO);
+			CurrentMapper ltCurrentMapper = new CurrentMapper(lowerTrestlesCurrent,ratingDTO,sunlightDTO,surfDTO,windDTO);
 
 			//Running methods to map fields to Current object
-			ltCurrentMapper.WaveQuality();
-			ltCurrentMapper.sunrise();
-			ltCurrentMapper.sunset();
+			ltCurrentMapper.SL_WaveQuality();
+			ltCurrentMapper.SL_Sunrise();
+			ltCurrentMapper.SL_Sunset();
+			ltCurrentMapper.SL_Surf();
+			ltCurrentMapper.SL_Wind();
 
-			SurfLine_surf_DTO surfDTO = sl_surf_service.getSurfLineSurf();
 			SurfLine_swells_DTO swellsDTO = lt_swells_service.getSurfLineSwells();
 			SurfLine_tides_DTO tidesDTO = lt_tides_service.getSurfLineTides();
-			SurfLine_wind_DTO windDTO = lt_wind_service.getSurfLineWind();
 			SurfLine_weather_DTO weatherDTO = lt_weather_service.getSurfLineWeather();
 
 			lt_ds_service.getDeepSwellCurrent();
 			lt_sc_service.getSurfCaptainCurrent();
 			lt_sf_service.getSurfForecastCurrent();
+
 
 			// Print out the filled DTO's
 //			System.out.println(ratingDTO);
--- src/main/java/com/surf/surftracker/mapper/CurrentMapper.java
+++ src/main/java/com/surf/surftracker/mapper/CurrentMapper.java
@@ -4,6 +4,7 @@ CURRENT CONDITIONS:                      MAPPER CLASS INFORMATION!!!
  */
 package com.surf.surftracker.mapper;
 
+import java.sql.Time;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -13,23 +14,29 @@ import java.util.NoSuchElementException;
 
 import com.surf.surftracker.dto.SurfLine_rating_DTO;
 import com.surf.surftracker.dto.SurfLine_sunlight_DTO;
+import com.surf.surftracker.dto.SurfLine_surf_DTO;
+import com.surf.surftracker.dto.SurfLine_wind_DTO;
 import com.surf.surftracker.model.Current;
 import com.surf.surftracker.util.TimeStampUtils;
 
 //@Scheduled
 
 public class CurrentMapper {
-    private Current currentSpot;
-    private SurfLine_rating_DTO currentRating;
-    private SurfLine_sunlight_DTO currentSunlightDTO;
+    private final Current currentSpot;
+    private final SurfLine_rating_DTO currentRatingDTO;
+    private final SurfLine_sunlight_DTO currentSunlightDTO;
+    private final SurfLine_surf_DTO currentSurfDTO;
+    private final SurfLine_wind_DTO currentWindDTO;
 
     Long nearestHour = TimeStampUtils.NearestHour();
     Long midnight = TimeStampUtils.Midnight();
 
-    public CurrentMapper(Current currentSpot, SurfLine_rating_DTO currentRatingDTO, SurfLine_sunlight_DTO currentSunlightDTO) {
+    public CurrentMapper(Current currentSpot, SurfLine_rating_DTO currentRatingDTO, SurfLine_sunlight_DTO currentSunlightDTO, SurfLine_surf_DTO currentSurfDTO, SurfLine_wind_DTO currentWindDTO) {
         this.currentSpot = currentSpot;
-        this.currentRating = currentRatingDTO;
+        this.currentRatingDTO = currentRatingDTO;
         this.currentSunlightDTO = currentSunlightDTO;
+        this.currentSurfDTO = currentSurfDTO;
+        this.currentWindDTO = currentWindDTO;
     }
 
     //@Scheduled
@@ -40,8 +47,8 @@ public class CurrentMapper {
     }
 
     //Current WaveQuality
-    public void WaveQuality() {
-        List<SurfLine_rating_DTO.Rating> ratings = currentRating.getData().getRating();
+    public void SL_WaveQuality() {
+        List<SurfLine_rating_DTO.Rating> ratings = currentRatingDTO.getData().getRating();
         for (SurfLine_rating_DTO.Rating rating : ratings) {
             if (Long.valueOf(rating.getTimestamp()).equals(nearestHour)) {
                 String currentRating = rating.getRatingDetails().getKey();
@@ -53,8 +60,34 @@ public class CurrentMapper {
         throw new NoSuchElementException("No rating found for the timestamp: " + nearestHour);
     }
 
+    //SurfLine Wave Height
+    public void SL_Surf() {
+        List<SurfLine_surf_DTO.Surf> surfList = currentSurfDTO.getData().getSurf();
+
+        for (SurfLine_surf_DTO.Surf surf : surfList) {
+            if (surf.getTimestamp() == TimeStampUtils.NearestHour()) {
+                SurfLine_surf_DTO.SurfDetails surfDetails = surf.getSurfDetails();
+
+                int min = (int)surfDetails.getMin();
+                int max = (int)surfDetails.getMax();
+                boolean plus = surfDetails.isPlus();
+                String plusSign = "";
+                if(plus){
+                    plusSign = "+";
+                }
+
+                String waveHeight = String.format("%d-%d%s ft",min,max,plusSign);
+                currentSpot.setSurfLineWaveHeight(waveHeight);
+                //System.out.println("SL Wave Height is: " + waveHeight);
+                return; // Exit after finding and processing the relevant surf details
+            }
+        }
+
+        throw new NoSuchElementException("No surf details found for the timestamp: " + TimeStampUtils.NearestHour());
+    }
+
     //Sunrise					    String 6:53AM >>> find previous midnight and then search for sunrise
-    public void sunrise() {
+    public void SL_Sunrise() {
         List<SurfLine_sunlight_DTO.Sunlight> sunlightList = currentSunlightDTO.getData().getSunlight();
         for (SurfLine_sunlight_DTO.Sunlight sun : sunlightList) {
             if (sun.getMidnight() == midnight) {
@@ -70,7 +103,7 @@ public class CurrentMapper {
     }
 
     //Sunset					        String 7:54PM >>> find previous midnight and then search for sunset
-    public void sunset() {
+    public void SL_Sunset() {
         List<SurfLine_sunlight_DTO.Sunlight> sunlightList = currentSunlightDTO.getData().getSunlight();
         for (SurfLine_sunlight_DTO.Sunlight sun : sunlightList) {
             if (sun.getMidnight() == midnight) {
@@ -88,7 +121,61 @@ public class CurrentMapper {
         }
         throw new NoSuchElementException("No sunrise found for the midnight timestamp: " + midnight);
     }
+
+
+    public void SL_Wind() {
+        List<SurfLine_wind_DTO.WindData.WindEntry> currentWind = currentWindDTO.getData().getWind();
+        long nearestHour = TimeStampUtils.NearestHour();
+        System.out.println("Nearest Hour: " + nearestHour);
+        for (SurfLine_wind_DTO.WindData.WindEntry wind : currentWind) {
+            System.out.println("Wind Entry Timestamp: " + wind.getTimestamp());
+            if (wind.getTimestamp() == TimeStampUtils.NearestHour()) {
+
+                double currentWindSpeed = wind.getSpeed();
+                int speed = (int)currentWindSpeed;
+                double direction =  wind.getDirection();
+                String formattedDirection = getCompassDirection((int) direction);
+                String windSpeed = String.format("%dkts %s", speed, formattedDirection);
+                System.out.println("Here is the current windspeed: " + windSpeed);
+                currentSpot.setWind(windSpeed);
+                return;
+            }
+        }
+        throw new NoSuchElementException("No wind found for the timestamp: " + TimeStampUtils.NearestHour());
+    }
+
+    private String getCompassDirection(int direction) {
+        String[] compassDirections = {"N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"};
+        int index = (int) Math.round(((double) direction % 360) / 22.5);
+        return compassDirections[index % 16];
+    }
+    /*
+    //Wind current				    String 5mph E
+    public void SL_Wind() {
+        List<SurfLine_wind_DTO.WindData.WindEntry> currentWind = currentWindDTO.getData().getWind();
+        for (SurfLine_wind_DTO.WindData.WindEntry wind : currentWind) {
+            if (wind.getTimestamp() == TimeStampUtils.NearestHour()) {
+                int speed = (int) wind.getSpeed();
+                int direction = (int)wind.getDirection();
+                String formattedDirection;
+
+                String.format("%fKTS %s",speed,formattedDirection);
+            }
+
+
+            currentSpot.setWind(windSpeed);
+            return;
+        }
+        throw new NoSuchElementException("No wind found for the timestamp: " + TimeStampUtils.NearestHour());
+    }
+*/
+//6 KTS E
+
+
 }
+
+
+
 
 /*
     //@Scheduled... every hour
@@ -102,14 +189,8 @@ currentSpot.setSurfLineWaveHeight();
 
 }
 */   /*
-    //SurfCaptain current		    String
-    public void surfCaptainWaveHeight(){}
 
-    //DeepSwell current			    String
-    public void deepSwellWaveHeight(){}
 
-    //Surf-Forecast current		    String
-    public void surfForecastWaveHeight(){}
 
     //Wind current				    String 5mph E
     public void wind(){}
